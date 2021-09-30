834763bf907e76a262cc80459106f10b7fde6a68
yungu0010 <dbstj16@sookmyung.ac.kr>
2021-09-30 16:46:05 +0900

    Getting the Location(yet permissions)

--- App.js
+++ App.js
@@ -1,10 +1,29 @@
 import React from 'react';
 import Loading from './Loading';
+import * as Location from 'expo-location';
 import { StyleSheet, Text, View } from 'react-native';
 
+
+/*함수형 컴포넌트
 export default function App() {
   return (
     <Loading></Loading>
   );
+}*/
+
+//클래스형 컴포넌트로 수정
+export default class extends React.Component{
+  // async-await 기다리라는 의미
+  getLocation = async() => {
+    const location = await Location.getCurrentPositionAsync();
+    console.log(location);
+  }
+
+  componentDidMount(){
+    this.getLocation(); 
+  }
+  render(){
+    return <Loading></Loading>;
+  }
 }
 
